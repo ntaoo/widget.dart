d768bc6eea20713d53a985a194970bf2ca883102
Kevin Moore <kevin@thinkpixellab.com>
2013-01-02 15:13:33 -0500

    HeaderedContentComponent: a decent implementation

--- lib/headered_content_component.dart
+++ lib/headered_content_component.dart
@@ -1,5 +1,9 @@
 import 'package:web_ui/web_ui.dart';
 
 class HeaderedContentComponent extends WebComponent {
+  String get header => attributes['header'];
 
+  void set header(String value) {
+    attributes['header'] = value;
+  }
 }
