326f0cedfdf8cdac630c04fda3392d570436d12d
Kevin Moore <kevin@thinkpixellab.com>
2013-01-04 12:35:22 -0500

    ExpanderComponent: trivial expand/collapse behavior

--- lib/components/expander_component.dart
+++ lib/components/expander_component.dart
@@ -1,13 +1,38 @@
+import 'dart:html';
 import 'package:web_ui/web_ui.dart';
 
 class ExpanderComponent extends WebComponent {
+  const String _expanderDivSelector = '#-x-expander-container';
+
+  bool _isExpanded = true;
+  DivElement _expanderDiv;
+
+  bool get isExpanded => _isExpanded;
+
   String get header => attributes['header'];
 
   void set header(String value) {
     attributes['header'] = value;
   }
 
+  void inserted() {
+    assert(_expanderDiv == null);
+    _expanderDiv = query(_expanderDivSelector);
+    assert(_expanderDiv != null);
+  }
+
+  void removed() {
+    assert(_expanderDiv != null);
+    _expanderDiv = null;
+  }
+
   void _toggle() {
-    print('toggle!');
+    assert(_expanderDiv != null);
+    if(_isExpanded) {
+      _expanderDiv.style.display = 'none';
+    } else {
+      _expanderDiv.style.display = '';
+    }
+    _isExpanded = !_isExpanded;
   }
 }
