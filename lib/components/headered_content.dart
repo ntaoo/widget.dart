2831d30f7076aeffa0a330bcb7631b85b19b1f01
Kevin Moore <kevin@thinkpixellab.com>
2013-01-12 18:03:47 -0500

    Moving HeaderedContent to the new <header> model

--- lib/components/headered_content.dart
+++ lib/components/headered_content.dart
@@ -1,9 +1,4 @@
 import 'package:web_ui/web_ui.dart';
 
 class HeaderedContent extends WebComponent {
-  String get header => attributes['header'];
-
-  void set header(String value) {
-    attributes['header'] = value;
-  }
 }
