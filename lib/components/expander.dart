39d23c751c01793574469837b0e057ea996553eb
Kevin Moore <kevin@thinkpixellab.com>
2013-01-13 22:01:15 -0500

    Expander: added an 'open' event

--- lib/components/expander.dart
+++ lib/components/expander.dart
@@ -1,10 +1,14 @@
 import 'dart:html';
+import 'package:bot/bot.dart';
 import 'package:web_ui/web_ui.dart';
 import 'package:widget/effects.dart';
 
 class Expander extends WebComponent {
-  const String _expanderDivSelector = '#expander-content-x';
+  static const String _openName = 'open';
+  static const String _expanderDivSelector = '#expander-content-x';
   static final ShowHideEffect _effect = new ShrinkEffect();
+
+  EventListenerList _onOpen;
 
   bool _isExpanded = true;
   DivElement _expanderDiv;
@@ -12,18 +16,44 @@ class Expander extends WebComponent {
 
   bool get isExpanded => _isExpanded;
 
+  void set isExpanded(bool value) {
+    if(value != _isExpanded) {
+      _isExpanded = value;
+      _updateElements();
+
+      if(_isExpanded) {
+        _raiseOpen();
+      }
+    }
+  }
+
+  EventListenerList get onOpen {
+    if(_onOpen == null) {
+      _onOpen = super.on[_openName];
+    }
+    return _onOpen;
+  }
+
+  void toggle() {
+    assert(_expanderDiv != null);
+    isExpanded = !isExpanded;
+  }
+
+  @protected
   void inserted() {
     assert(_expanderDiv == null);
     _expanderDiv = this.query(_expanderDivSelector);
     assert(_expanderDiv != null);
+    _updateElements(true);
 
     assert(_header == null);
     _header = this.query('header');
     if(_header != null) {
-      _header.on.click.add((_) => _toggle());
+      _header.on.click.add((_) => toggle());
     }
   }
 
+  @protected
   void removed() {
     assert(_expanderDiv != null);
     _expanderDiv = null;
@@ -32,10 +62,17 @@ class Expander extends WebComponent {
     _header = null;
   }
 
-  void _toggle() {
-    assert(_expanderDiv != null);
-    final action = _isExpanded ? ShowHideAction.HIDE : ShowHideAction.SHOW;
-    ShowHide.begin(action, _expanderDiv, effect: _effect);
-    _isExpanded = !_isExpanded;
+  // DARTBUG:
+  // TODO: Need a way to dispatch my own events
+  void _raiseOpen() {
+    onOpen.dispatch(new Event(_openName));
+  }
+
+  void _updateElements([bool skipAnimation = false]) {
+    if(_expanderDiv != null) {
+      final action = _isExpanded ? ShowHideAction.SHOW : ShowHideAction.HIDE;
+      final effect = skipAnimation ? null : _effect;
+      ShowHide.begin(action, _expanderDiv, effect: effect);
+    }
   }
 }
