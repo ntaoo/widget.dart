670b73dae4bbb99d3880b2ac56a7bebce8986ac4
Kevin Moore <kevin@thinkpixellab.com>
2013-01-14 17:15:35 -0500

    Tabs: starting

---
+++ lib/components/tabs.dart
@@ -0,0 +1,14 @@
+import 'dart:html';
+import 'package:web_ui/web_ui.dart';
+import 'package:widget/effects.dart';
+import 'package:bot/bot.dart';
+
+class Tabs extends WebComponent {
+  @protected
+  void created() {
+  }
+
+  @protected
+  void inserted() {
+  }
+}
