82ca57961b4315a3ffeebfefaf40ac24d928d8c5
Kevin Moore <kevin@thinkpixellab.com>
2013-01-16 16:37:58 -0500

    Tests: Tools: testing % width and height
    ...hmm...

--- test/effects/tools_tests.dart
+++ test/effects/tools_tests.dart
@@ -1,5 +1,7 @@
 part of effects_tests;
 
+final _containerSize = const Size(100, 80);
+
 void registerToolsTests() {
   group('Tools', () {
     setUp(() {
@@ -16,8 +18,15 @@ void registerToolsTests() {
         final pg = _getPlayground();
 
         pg.appendHtml('''
-            <style scoped>div.foo { $css }</style>
-            <div class='foo'>content</div>
+<div class='container'>
+<style scoped>
+div.container { 
+  background: gray; 
+  width: ${_containerSize.width}px; height: ${_containerSize.height}px;
+}
+div.foo { background: red; $css }
+</style>
+<div class='foo'>content</div>
         ''');
 
         final expectCallback = expectAsync1((Size size) {
@@ -26,6 +35,10 @@ void registerToolsTests() {
 
         pg.query('div.foo').getComputedStyle('')
         .transform((css) => Tools.getOuterSize(css))
+        .chain((value) {
+          return getTimeoutFuture(1)
+              .transform((_) => value);
+        })
         .then(expectCallback);
 
       });
@@ -41,5 +54,8 @@ final samples =
  'width: 10px; height: 8px; border: 1px solid;': new Size(12, 10),
  'width: 10px; height: 8px; padding: 2px;': new Size(14, 12),
  'width: 10px; height: 8px; padding: 2px; border: 1px;': new Size(14, 12),
- 'width: 10px; height: 8px; padding: 2px; border: 1px solid;': new Size(16, 14)
+ 'width: 10px; height: 8px; padding: 2px; border: 1px solid;': new Size(16, 14),
+ 'width: 100%; height: 8px;': new Size(_containerSize.width, 8),
+ 'width: 100%; height: 100%;': new Size(_containerSize.width, _containerSize.height),
+ 'width: 50%; height: 10%;': new Size(0.5 * _containerSize.width, 0.1 * _containerSize.height),
 };
