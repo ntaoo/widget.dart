c657abd659a4e47096bff918c18bc7082d41c622
Kevin Moore <kevin@thinkpixellab.com>
2013-01-18 16:18:02 -0500

    Flash TOC item when navigated to

--- web/app.dart
+++ web/app.dart
@@ -1,4 +1,27 @@
 import 'dart:html';
 
 void main() {
+  window.on.hashChange.add(_onNavigate);
 }
+
+void _onNavigate(HashChangeEvent e) {
+  final matches = _hashBitRegEx.firstMatch(e.newUrl);
+  if(matches != null) {
+    final elementId = matches[1];
+
+    final element = query('#$elementId');
+    if(element != null) {
+      _flashElement(element);
+    }
+  }
+}
+
+void _flashElement(Element element) {
+  element.classes.add(_highlightedClass);
+  window.setTimeout(() => element.classes.remove(_highlightedClass), 1000);
+}
+
+const _highlightedClass = 'highlighted';
+
+// these are the rules applied by build.dart
+final _hashBitRegEx = new RegExp(r'.*#([a-z_]+)');
