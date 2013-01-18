429438851cf900feff40eab724bac34069ecc3c9
Kevin Moore <kevin@thinkpixellab.com>
2013-01-18 14:27:00 -0500

    Swap: do init 'on demand' so that state can be accessed earlier in component lifecycle
    See https://github.com/dart-lang/web-ui/issues/226

--- lib/components/swap.dart
+++ lib/components/swap.dart
@@ -12,7 +12,8 @@ class Swap extends WebComponent implements SwapComponent {
   static const _activeClass = 'active';
   static const _dirClassPrev = 'prev';
 
-  Element _contentElement;
+  // should only be accessed via the [_contentElement] property
+  Element _contentElementField;
 
   int get activeItemIndex {
     return items.indexOf(activeItem);
@@ -58,30 +59,47 @@ class Swap extends WebComponent implements SwapComponent {
 
   @protected
   void inserted() {
-    _contentElement = this.query('x-swap > .content');
-    assert(_contentElement != null);
     _initialize();
   }
 
-  void _initialize() {
-    // if there are any elements, make sure one and only one is 'active'
-    final activeFigures = new List<Element>.from(items.filter((e) => e.classes.contains(_activeClass)));
-    if(activeFigures.length == 0) {
-      if(items.length > 0) {
-        // marke the first of the figures as active
-        items[0].classes.add(_activeClass);
-      }
-    } else {
-      activeFigures.getRange(1, activeFigures.length - 1)
-        .forEach((e) => e.classes.remove(_activeClass));
-    }
+  @protected
+  void removed() {
+    _contentElementField = null;
+  }
 
-    // A bit of a hack. Because we call Swap w/ two displayed items:
-    // one marked 'prev' and one marked 'next', Swap tries to hide one of them
-    // this only causes a problem when clicking right the first time, since all
-    // times after, the cached ShowHideState of the item is set
-    // So...we're going to walk the showHide states of all children now
-    // ...and ignore the result...but just to populate the values
-    items.forEach((f) => ShowHide.getState(f));
+  Element get _contentElement {
+    _initialize();
+    return _contentElementField;
+  }
+
+  void _initialize() {
+    if(_contentElementField == null) {
+      _contentElementField = this.query('x-swap > .content');
+      if(_contentElementField == null) {
+        throw 'Could not find the content element. Either the template has changed or state was accessed too early in the component lifecycle.';
+      }
+
+      final theItems = _contentElementField.children;
+
+      // if there are any elements, make sure one and only one is 'active'
+      final activeFigures = new List<Element>.from(theItems.filter((e) => e.classes.contains(_activeClass)));
+      if(activeFigures.length == 0) {
+        if(theItems.length > 0) {
+          // marke the first of the figures as active
+          theItems[0].classes.add(_activeClass);
+        }
+      } else {
+        activeFigures.getRange(1, activeFigures.length - 1)
+          .forEach((e) => e.classes.remove(_activeClass));
+      }
+
+      // A bit of a hack. Because we call Swap w/ two displayed items:
+      // one marked 'prev' and one marked 'next', Swap tries to hide one of them
+      // this only causes a problem when clicking right the first time, since all
+      // times after, the cached ShowHideState of the item is set
+      // So...we're going to walk the showHide states of all children now
+      // ...and ignore the result...but just to populate the values
+      theItems.forEach((f) => ShowHide.getState(f));
+    }
   }
 }
