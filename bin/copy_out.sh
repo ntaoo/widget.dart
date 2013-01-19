82c9f11bd5258e875008f3379e413a03ede88cbb
Kevin Moore <kevin@thinkpixellab.com>
2013-01-19 10:44:33 -0500

    quote the replace with value

--- bin/copy_out.sh
+++ bin/copy_out.sh
@@ -8,6 +8,6 @@ rm $WEB_DIR/*.dart
 
 # now we need to fix up the relative URL crazy...hmm...
 TO_FIND='href="..\/style.css"'
-REPLACE_WITH=href="style.css"
+REPLACE_WITH='href="style.css"'
 COMMAND=s/$TO_FIND/$REPLACE_WITH/g
 sed -e $COMMAND $BUILD_DIR/index.html > $WEB_DIR/index.html
