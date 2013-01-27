fd28a5f9246e0a70d7d021ee938f544aa6240ddf
Kevin Moore <kevin@thinkpixellab.com>
2013-01-27 12:59:11 -0500

    Revert "interesting..."
    This reverts commit 24d5be0558edc3dfa7609dc1059031a23d7a6b0b.

--- lib/components/accordion.dart
+++ lib/components/accordion.dart
@@ -4,9 +4,6 @@ import 'package:widget/effects.dart';
 import 'package:widget/widget.dart';
 import 'package:bot/bot.dart';
 
-/**
- * A cool component...
- */
 class Accordion extends WebComponent {
   @protected
   void created() {
