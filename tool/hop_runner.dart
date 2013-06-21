86fde354426699246104de9509b76fa6b170ba35
Kevin Moore <kevin@thinkpixellab.com>
2013-06-21 17:43:27 -0400

    Updated hop_runner
    removed docs task - and other unused content
    fixed line lengths

--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-import 'dart:io';
 import 'package:bot/bot.dart';
 import 'package:hop/hop.dart';
 import 'package:hop/hop_tasks.dart';
@@ -8,15 +6,15 @@ import 'util.dart' as util;
 import '../test/test_dump_render_tree.dart' as test_drt;
 
 void main() {
-  final buildTask = createProcessTask('dart', args: ['build.dart'],
-      description: "execute the project's build.dart file");
-  addTask('build', buildTask);
+  addTask('build', createProcessTask('dart', args: ['build.dart'],
+      description: "execute the project's build.dart file"));
 
   final paths = ['web/out/index.html_bootstrap.dart'];
-  addTask('dart2js', createDart2JsTask(paths,
+  addTask('dart2js', createDartCompilerTask(paths,
       minify: true, liveTypeAnalysis: true, rejectDeprecatedFeatures: true));
 
-  addTask('test_dart2js', createDart2JsTask(['test/browser_test_harness.dart']));
+  addTask('test_dart2js',
+      createDartCompilerTask(['test/browser_test_harness.dart']));
 
   addTask('test', createUnitTestTask(test_drt.testCore));
 
@@ -29,17 +27,8 @@ void main() {
   //
   // populate components into example dir
   //
-  addAsyncTask('copy_components', (ctx) => startProcess(ctx, './bin/copy_out.sh'));
-
-
-  addTask('docs', createDartDocTask(_getLibraryPaths, targetBranch: 'docs'));
+  addAsyncTask('copy_components', (ctx) =>
+      startProcess(ctx, './bin/copy_out.sh'));
 
   runHop();
-}
-
-List<String> _getLibraryPaths() {
-  final libLocations = [r'lib/', r'lib/components'];
-  return util.getDartLibraryPaths()
-    .map((Path p) => p.toString())
-    .toList();
 }
